f39a5bc70a3d781b0e9faeeb9b034a99e266a040
Maycon Ricardo Oliveira <maycon.oliveira@leiturinha.com.br>
2021-07-16 23:01:41 -0300

    Chalenge completed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,18 +6,39 @@ import { Task, TasksList } from '../components/TasksList';
 import { TodoInput } from '../components/TodoInput';
 
 export function Home() {
+
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+
+    const data = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false,
+    }
+
+    setTasks(oldState => [...oldState, data]);
+
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+
+    const updatedTasks = tasks.map(task => ({ ...task }));
+
+    updatedTasks.find(task => {
+      if (task.id == id) {
+        task.done = true;
+      }
+    })
+
+    setTasks(updatedTasks);
+
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(oldState => oldState.filter(
+      task => task.id != id
+    ));
   }
 
   return (
@@ -26,10 +47,10 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <TasksList 
-        tasks={tasks} 
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
       />
     </View>
   )
